e400df936e063d41585bcd9ff333dfe8894e5210
Shashikant86 <shashikant.jagtap@hotmail.co.uk>
2013-12-21 20:00:53 +0000

    Poltergeist Network traffic

---
+++ Gemfile
@@ -0,0 +1,22 @@
+source :rubygems
+
+gem "sinatra"
+gem "shotgun"
+gem 'json'
+gem "capybara"           
+gem "cucumber"
+gem "rspec", :require => "spec"
+gem "parallel_tests", "~> 0.12"
+gem "poltergeist"
+gem "selenium-webdriver"
+gem "rest-client"
+gem "json_pure"
+gem 'nokogiri', '~> 1.6.0'
+
+
+	
+
+
+
+
+
---
+++ features/support/env.rb
@@ -0,0 +1,21 @@
+ 
+require "Capybara"
+require "Capybara/cucumber"
+require "rspec"
+require 'capybara/poltergeist'
+
+Capybara.default_driver = :poltergeist
+Capybara.register_driver :poltergeist do |app|
+
+    options = {
+        :js_errors => true,
+        :timeout => 120,
+        :debug => false,
+        :phantomjs_options => ['--load-images=no', '--disk-cache=false'],
+        :inspector => true,
+
+    }
+
+    Capybara::Poltergeist::Driver.new(app, options)
+end
+
